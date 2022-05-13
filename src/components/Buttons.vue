1528b02c52afcf9d410b966dfaa2b82eaf0781f4
Lien-YT <lienyuting@yahoo.com.tw>
2022-05-13 21:12:35 +0800

    Buttons style settings

---
+++ src/components/Buttons.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="buttons-control">
+    <button class="btn btn-outline">
+      <div class="prev-btn">
+        <img src="./../assets/images/arrow-left-black.png"/>上一步
+      </div>
+    </button>
+    <button
+      class="btn next-btn btn-primary"
+    >
+      下一步<img src="./../assets/images/arrow-right.png"/>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.buttons-control {
+  padding: 1.5rem 0;
+  border-top: 1px solid #e6e6eb;
+  display: flex;
+  justify-content: space-between;
+  grid-column: 1/7;
+  grid-row: 4/5;
+}
+.btn {
+  height: 46px;
+  min-width: 156px;
+  width: 100%;
+}
+.prev-btn {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+}
+.next-btn {
+  margin-left: 1rem;
+  border-radius: 8px;
+  background-color: #f67599;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+img {
+  height: 9px;
+  width: 21px;
+}
+.prev-btn > img {
+  margin-right: 0.75rem;
+}
+.next-btn > img {
+  margin-left: 0.75rem;
+}
+</style>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -2,20 +2,23 @@
   <div class="checkout">
     <h2>結帳</h2>
     <Steppers />
+    <Buttons />
     <Cart />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Cart from "@/components/Cart";
 import Steppers from "@/components/Steppers";
+import Buttons from "@/components/Buttons";
+import Cart from "@/components/Cart";
 
 export default {
   name: "Checkout",
   components: {
+    Steppers,
+    Buttons,
     Cart,
-    Steppers
   },
 };
 </script>
